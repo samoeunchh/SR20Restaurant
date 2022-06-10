0c94e4a628d27461a925670392a617262f6defde
samoeunchh <samoeunchh@gmail.com>
2022-06-10 19:17:26 +0700

    First commit

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        [Display(Name ="Customer Name")]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [Phone]
+        public string Phone { get; set; }
+        [MaxLength(100)]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+    }
+}
